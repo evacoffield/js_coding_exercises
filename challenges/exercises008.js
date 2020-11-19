63150433070b946bcc3953e00c07e18757e32089
Eva Coffield <eva@darcoff.com>
2020-11-19 21:44:00 +0000

    Implemented exersises008 - fizzBuzz with an Array containing 1 through n numbers

---
+++ challenges/exercises008.js
@@ -0,0 +1,28 @@
+function fizzBuzz(n) {
+    const foo = [...Array(n).keys()];
+    foo.forEach( i => {
+        i++;
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    })
+
+}
+fizzBuzz(15);
+
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    
+    // Write the second line of output that prints the contents of 'inputString' here.
+    let secondLine = inputString;
+    console.log(secondLine);
+}
+processData("Welcome to 30 Days of Code!");
+
